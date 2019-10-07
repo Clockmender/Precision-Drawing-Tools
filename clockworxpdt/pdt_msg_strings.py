14348fa69fbcbd9c33ab26180e486a0e5c2d8784
Alan Odom <clockmender@icloud.com>
2019-10-07 18:18:12 +0100

    Update messages file

---
+++ clockworxpdt/pdt_msg_strings.py
@@ -0,0 +1,72 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+#
+# ----------------------------------------------------------
+# Authors: Alan Odom (Clockmender), Rune Morling (ermo)
+# ----------------------------------------------------------
+#
+# English messages
+
+PDT_ERR_NO_ACT_OBJ    = "No Active Object - Please Select an Object"
+PDT_ERR_NO_ACT_VERTS  = "No Selected Geometry - Please Select some Geometry"
+PDT_ERR_ONE_EDGE      = "Select Only One Edge (You Selected: "
+PDT_ERR_NON_VALID     = " is Not a Valid Option for Command: "
+PDT_ERR_VERT_MODE     = "Work in Vertex Mode for this Function"
+
+PDT_ERR_SEL_1_VERTI   = "Select at least 1 Vertex Individually (You selected: "
+PDT_ERR_SEL_1_VERT    = "Select at least 1 Vertex (You selected: "
+PDT_ERR_SEL_1_EDGE    = "Select at least 1 Edge (You selected: "
+PDT_ERR_SEL_2_VERTI   = "Select at least 2 Vertices Individually (You selected: "
+PDT_ERR_SEL_2_VERTIO  = "Select Exactly 2 Vertices Individually (You selected: "
+PDT_ERR_SEL_2_VERTS   = "Select Exactly 2 Vertices (You selected: "
+PDT_ERR_SEL_3_VERTS   = "Select Exactly 3 Vertices (You selected: "
+PDT_ERR_SEL_3_VERTIO  = "Select Exactly 3 Vertices Individually (You selected: "
+PDT_ERR_SEL_4_VERTS   = "Select 4 Vertices Individually, or 2 Edges (You Selected: "
+
+PDT_ERR_SEL_2_OBJS    = "Select Exactly 2 Objects (You selected: "
+PDT_ERR_SEL_3_OBJS    = "Select Exactly 3 Objects (You selected: "
+PDT_ERR_SEL_4_OBJS    = "Select Exactly 4 Objects (You selected: "
+
+PDT_ERR_FACE_SEL      = "You have a Face Selected, this would have ruined the Topology"
+
+PDT_ERR_INT_LINES     = "Implied Lines Do Not Intersect in "
+PDT_ERR_INT_NO_ALL    = "Active Vertex was not Closest to Intersection and All/Act was not Selected"
+PDT_ERR_STRIGHT_LINE  = "Selected Points all lie in a Straight Line"
+PDT_ERR_CONNECTED     = "Vertices are already Connected"
+PDT_ERR_EDIT_MODE     = "Only Works in Edit Mode (You are in: )"
+PDT_ERR_EDOB_MODE     = "Only Works in Edit, or Object Modes (You are in: )"
+PDT_ERR_TAPER_ANG     = "Angle must be in Range -80 to +80 (You have it set to: "
+PDT_ERR_TAPER_SEL     = "Select at Least 2 Vertices Individually - Active is Rotation Point (You selected: "
+
+# Info messages
+#
+PDT_INF_OBJ_MOVED     = "Active Object Moved to Intersection, "
+
+# Descriptions
+#
+PDT_DES_XDELTA        = "X Coord Delta"
+PDT_DES_YDELTA        = "Y Coord Delta"
+PDT_DES_ZDELTA        = "Z Coord Delta"
+PDT_DES_OFFDIS        = "Offset Distance"
+PDT_DES_OFFANG        = "Offset Angle"
+PDT_DES_OFFPER        = "Offset Percentage"
+PDT_DES_WORPLANE      = "Choose Working Plane"
+PDT_DES_MOVESEL       = "Select Move Mode"
+PDT_DES_OPMODE        = "Select Operation Mode"
+PDT_DES_ROTMOVAX      = "Rotational Axis - Movement Axis"
